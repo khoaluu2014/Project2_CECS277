1bff0d9454da2e935c1d0295a3d5bc70ca7c1e87
khoaluu2014 <43567901+khoaluu2014@users.noreply.github.com>
2021-11-28 02:09:53 -0800

    Revert "Delete specific Pokemon class"

---
+++ Pokemon/Ponyta.java
@@ -0,0 +1,108 @@
+/*
+@Joshua Peng
+*/
+
+/**
+ * Class Ponyta is a type of Pokemon and inherits variables and methods from 
+ * Pokemon class. Class Ponyta also implements Fire interface and calls 
+ * the variables and methods for the special attacks which are based on its fire type.
+ * @author Joshua Peng
+ */
+/*
+public class Ponyta extends Pokemon implements Fire{
+	public Ponyta() {
+    /**
+	   * Calls superclass Pokemon's constructor and assigns "Ponyta" for name of pokemon.
+		 * @param "Ponyta" name of trainer's pokemon or wild pokemon.
+		 */
+/*
+		super("Ponyta");  		  
+  }
+
+  /**
+   * Gets special menu for the fire special attacks by returning the specialMenu variable
+   * created in Fire interface.
+   * return@ string menu of special attacks for fire type.
+   */
+/*
+  public String getSpecialMenu() {
+	  return specialMenu;
+  }
+
+  /**
+   * Gets the number of items in the menu by returning the numSpecialMenuItems variable created
+   * in Fire interface. Used in Main class to set the upper range for special attacks menu choices. 
+   * return@ number of items in the special attack menu.
+   */
+/*
+  public int getNumSpecialMenuItems() {
+	  return  numSpecialMenuItems;
+  }
+  
+  /**
+   * Gives the battle description when trainer chooses Ember or when wild pokemon randomly 
+   * chooses Ember for special attack, and calculates the amount of damage that wild pokemon takes,
+   * as well as the damage taken on the trainer's pokemon.
+   * @param p wild pokemon that trainer's pokemon is fighting with.
+   * @return string summary of battle description.
+   */
+/*
+  public String ember(Pokemon p) {
+	  String ember = this.getName() + " uses EMBER on " + p.getName();
+	  double damage = Rand.randIntRange(0,4) * this.damageMultiplier(p);
+	  p.takeDamage((int)damage);
+	  return ember; 
+  }
+
+  /**
+   * Gives the battle description when trainer chooses Fire Blast or when wild pokemon randomly 
+   * chooses Fire Blast for special attack, and calculates the amount of damage that wild pokemon takes,
+   * as well as the damage taken on the trainer's pokemon.
+   * @param p wild pokemon that trainer's pokemon is fighting with.
+   * @return string summary of battle description.
+   */
+/*
+  public String fireBlast(Pokemon p) {
+	  String fireBlast = this.getName() + " uses FIRE BLAST on " + getName();
+	  double damage = Rand.randIntRange(2,5) * this.damageMultiplier(p);
+	  p.takeDamage((int)damage);
+	  return fireBlast; 
+  }
+
+  /**
+   * Gives the battle description when trainer chooses Fire Punch or when wild pokemon randomly 
+   * chooses Fire Punch for special attack, and calculates the amount of damage that wild pokemon takes,
+   * as well as the damage taken on the trainer's pokemon.
+   * @param p wild pokemon that trainer's pokemon is fighting with.
+   * @return string summary of battle description.
+   */
+/*
+  public String firePunch(Pokemon p) {
+	  String firePunch = this.getName() + " uses FIRE PUNCH on " + p.getName();
+	  int min = 1;
+	  int max = 4;
+	  double damage = Rand.randIntRange(1,4) * this.damageMultiplier(p);
+	  p.takeDamage((int)damage);
+	  return firePunch;
+  }
+
+  /**
+   * Gets the special attack description for the trainer's pokemon or the wild pokemon based on the 
+   * trainer's menu choice for special attack move or based on the wild pokemon's random menu choice.
+   * @param pokemon selected by trainer or wild pokemon that trainer encounters.
+   * @return string summary of battle description based on move type by calling special attack method.
+   */
+/*
+  public String specialAttack(Pokemon p, int move) {
+	  if (move == 1) {
+			 return ember(p);
+		 }	 
+		 else if (move == 2) {	
+			 return fireBlast(p);
+		 }
+		 else if (move == 3) {
+			 return firePunch(p); 
+		 }
+		 return "a";
+	  } 
+}
